f74a01933d39afe979a00ca61304ca5bfe9d1597
duke <duke@bang.local>
2010-10-09 16:51:44 -0400

    updated for the new :repositories syntax in lein and added the required vimclojure/server dep

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A leiningen plugin to launch a vimclojure nailgun server."
   :url "http://github.com/brandonw/lein-nailgun"
   :dependencies [[org.clojars.gilbertl/vimclojure "2.1.2"]
-                 [org.apache.maven/maven-ant-tasks "2.0.10"]]
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]
+                 [vimclojure/server "2.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
-  :repositories [["clojars" "http://clojars.org/repo"]])
+  :repositories {"clojars" "http://clojars.org/repo"})
 
