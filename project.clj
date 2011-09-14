6ea0c56ed15261eb5745ff21c0e475e1a2bfbb5f
scott <juveduke@gmail.com>
2011-09-14 13:15:58 -0400

    changed over to my clojars/github from brandon's

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.brandonw/lein-nailgun "1.1.0"
+(defproject org.clojars.scott/lein-nailgun "1.1.0"
   :description "A leiningen plugin to launch a vimclojure nailgun server."
-  :url "http://github.com/brandonw/lein-nailgun"
+  :url "http://github.com/mrowl/lein-nailgun"
   :dependencies [[org.clojars.gilbertl/vimclojure "2.1.2"]
                  [vimclojure/server "2.2.0"]]
   :dev-dependencies [[lein-clojars "0.7.0"]]
