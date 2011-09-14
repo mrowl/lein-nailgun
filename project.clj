d938e42556f10c4db9657bab8cc4078048690dd4
scott <juveduke@gmail.com>
2011-09-14 13:06:28 -0400

    removed the maven dependency (not sure why it was there to begin with)

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.brandonw/lein-nailgun "1.0.0"
+(defproject org.clojars.brandonw/lein-nailgun "1.1.0"
   :description "A leiningen plugin to launch a vimclojure nailgun server."
   :url "http://github.com/brandonw/lein-nailgun"
   :dependencies [[org.clojars.gilbertl/vimclojure "2.1.2"]
-                 [org.apache.maven/maven-ant-tasks "2.0.10"]
                  [vimclojure/server "2.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]]
   :repositories {"clojars" "http://clojars.org/repo"})
 
